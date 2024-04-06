a00deb4da5a0c446d621a5b18cd398c4d503ea2f
Rizky Fauzi Ilmi <rizkyfauziilmi@gmail.com>
2024-04-06 20:27:56 +0700

    Update midtrans.ts

--- lib/midtrans.ts
+++ lib/midtrans.ts
@@ -1,7 +1,7 @@
 import { MidtransClient } from 'midtrans-node-client';
 
 export const snap = new MidtransClient.Snap({
-  isProduction: true,
+  isProduction: false,
   serverKey: process.env.MIDTRANS_SERVER_KEY,
   clientKey: process.env.MIDTRANS_CLIENT_KEY,
 });
